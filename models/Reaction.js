769d86b57377157669fb898dbf65bc82093df43f
Sajjad Zaidi <sajjadzaidi2015@gmail.com>
2022-07-22 19:01:45 -0400

    Updating the model

---
+++ models/Reaction.js
@@ -0,0 +1,25 @@
+const {Schema, model} = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        text: {
+            type: String,
+            minLength: 15,
+            maxLength: 500,
+        },
+    },
+);
+
+module.exports = reactionSchema;
--- models/User.js
+++ models/User.js
@@ -15,43 +15,56 @@ const userSchema = new Schema(
             match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
             unique: true
         },
-        // thoughts: [ 
-        //     {
-        //     type: Schema.Types.ObjectId,
-        //     ref: 'Thought'
-        //     }
-        // ],
-        // friends: [
-        //     {
-        //     type: Schema.Types.ObjectId,
-        //     ref: 'User'
-        //     }
-        // ]
+        thoughts: [ 
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+          virtuals: true,
+        },
+        id: false,
     }
 )
 
-const User = model('User', userSchema);
-
-const handleError = (err) => console.error(err);
 
 
-
-User.find({}).exec((err, collection) => {
-    if (err) {
-      return handleError(err);
-    }
-    if (collection.length === 0) {
-      return User.insertMany(
-        [
-          { 'username': 'sajjad','email': 'sajjad@gmail.com'},
-          { 'username': 'Hamza','email': 'Hamza@gmail.com' },
-        ],
-        (insertError) =>
-          insertError ? handleError(insertError) : console.log('Inserted')
-      );
-    }
-    return console.log('Already populated');
+// Create a virtual called `friendCount` that retrieves the length of the user's `friends` array field on query.
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
   });
+
+// Initialize our User model
+const User = model('User', userSchema);  
+// const handleError = (err) => console.error(err);
+
+
+
+// User.find({}).exec((err, collection) => {
+//     if (err) {
+//       return handleError(err);
+//     }
+//     if (collection.length === 0) {
+//       return User.insertMany(
+//         [
+//           { 'username': 'sajjad','email': 'sajjad@gmail.com'},
+//           { 'username': 'Hamza','email': 'Hamza@gmail.com' },
+//         ],
+//         (insertError) =>
+//           insertError ? handleError(insertError) : console.log('Inserted')
+//       );
+//     }
+//     return console.log('Already populated');
+//   });
 
 
 module.exports = User;
